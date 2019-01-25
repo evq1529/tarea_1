974fa5e4cd2f328c9fd4065ebc94acf399a605ee
evq1529 <46851098+evq1529@users.noreply.github.com>
2019-01-25 12:19:20 -0600

    Tarea 1

---
+++ tarea_1.py
@@ -0,0 +1,8 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> PI = math.pi
+>>> ast = "******"
+>>> print("%.1s" %ast, "%.7f" %PI, "%.2s" %ast, "%.6f" %PI, "%.3s" %ast, "%.5f" %PI, "%.4s" %ast, "%.4f" %PI, "%.5s" %ast, "%.3f" %PI, "%.6s" %ast, "%.2f" %PI)
+* 3.1415927 ** 3.141593 *** 3.14159 **** 3.1416 ***** 3.142 ****** 3.14
+>>> 
